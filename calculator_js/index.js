844712be0bea880157055ec2c617bb44949263ce
Els <Emathivannan@spartaglobal.com>
2018-08-08 01:19:31 +0100

    Added advanced calculator

--- calculator_js/index.js
+++ calculator_js/index.js
@@ -4,10 +4,47 @@ var calc_choice = prompt("(b)asic, (a)dvnaced, (i)bmi or (t)rip");
 // If the input is 'b' the basic calculator prompt is run
 if(calc_choice=="b"){
   alert("Basic Calculator");
-// If the input is 'a' the advanced calculator prompt is run
+// Giving the code information so a basic calculation can be made
+  var num1=parseInt(prompt("Enter a value", "0"));
+  var operator=prompt("Enter an operator","eg. +, -, * or /");
+  var num2=parseInt(prompt("Enter a value", "0"));
+// Using switch to differentiate between the different operators
+  switch(operator){
+    case "+" :
+      alert(num1+num2);
+      break;
+    case "-" :
+      alert(num1-num2);
+      break;
+    case "*" :
+      alert(num1*num2);
+      break;
+    case "/" :
+      alert(num1/num2);
+      break;
+    default:
+      alert("Incorrect input");
+  }
 }
+// If the input is 'a' the advanced calculator prompt is run
 else if(calc_choice=="a"){
   alert("Advanced Calculator");
+// Asking the user if they would like to use the Power / Square root funtion
+  var adv_operator=prompt("Would you like the (p)ower or (s)quare root of initial input");
+// Using switch to differentiate between the different operators
+  switch (adv_operator) {
+    case "p":
+      num1=parseInt(prompt("Enter a value", "0"));
+      num2=parseInt(prompt("Enter a value", "0"));
+      alert(Math.pow(num1, num2));
+      break;
+    case "s":
+      num1=parseInt(prompt("Enter a value", "0"))
+      alert(Math.sqrt(num1));
+      break;
+    default:
+      alert("Incorrect input");
+  }
 }
 // if the input is "i" the BMT calculator prompt is run
 else if(calc_choice=="i"){
